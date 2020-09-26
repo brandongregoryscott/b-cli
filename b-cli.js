a16c6fecbca7e338c9aee7fd6236ad71f56e8988
Brandon Scott <brandongregoryscott@gmail.com>
2020-09-26 12:09:00 -0400

    Cleanup entrypoint to remove unnecessary example code

--- b-cli.js
+++ b-cli.js
@@ -4,8 +4,8 @@
 // #region Imports
 // -----------------------------------------------------------------------------------------
 
-const baseCommands = require("and-cli/modules/commands");
 const commandRegistry = require("and-cli/modules/command-registry");
+const packageConfig = require("and-cli/modules/package-config");
 const program = require("and-cli");
 
 // #endregion Imports
@@ -14,51 +14,13 @@ const program = require("and-cli");
 // #region Entrypoint
 // -----------------------------------------------------------------------------------------
 
-// Override the description from the and-cli
-program.description(
-    "Sandbox project to showcase extending functionality of and-cli"
-);
+const { description } = packageConfig.getLocal();
+program.description(description);
 
 // Register all of the base commands from the and-cli with this application
-commandRegistry.registerBaseCommands();
-
-// Register a single base command from the and-cli with this application
-commandRegistry.registerBaseCommand("dotnet", true);
-
-// Or, strongly typed with the command definitions
-commandRegistry.registerBaseCommand(baseCommands.dotnet.command, true);
-
-// The command registry also provides a function for registering multiple commands at once
-commandRegistry.registerCommands([
-    {
-        command: "example",
-        description: "Some example command",
-    },
-]);
-
-// Aliases can be registered to provide an easier way to type or remember a command and/or option(s)
-// The 'command' is the alias, and the 'description' is the command and option(s) that it should be mapped to.
 commandRegistry
-    .registerAlias({
-        command: "d",
-        description: "dotnet",
-    })
-    .registerAlias({
-        command: "dcRb",
-        description: "dotnet -cRb",
-    });
-
-// Aliases can be registered from the package.json as well - see the 'and-cli' section for the aliased
-// example command.
-commandRegistry.registerAliasesFromConfig();
-
-// Both this function and registerAlias() have an 'overrideIfTrue' parameter to opt-in to overwriting
-// of a registered command of the same name, if desired.
-commandRegistry.registerAliasesFromConfig(true);
-
-// Note - in order to preprocess the command line args, you must call commandRegistry.parseWithAliases()
-// instead of just program.parse()! Without calling this, the command string will be interpreted literally
-// and will likely fall through to the 'unknown command' error.
-commandRegistry.parseWithAliases();
+    .registerBaseCommands()
+    .registerAliasesFromConfig()
+    .parseWithAliases();
 
 // #endregion Entrypoint
